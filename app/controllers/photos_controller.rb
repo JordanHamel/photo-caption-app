150b3d04d3a82e3a720d5fc2c7d56305d74eb9be
Jordan Hamel <jo.mravca@gmail.com>
2013-02-28 07:53:47 -0800

    buttons look like you're pushing them down

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,11 +1,13 @@
 class PhotosController < ApplicationController
 	before_filter :authenticate_user!
 
+
 	def new
 		@photo = Photo.new
 	end
 
 	def create
+		before_filter :authenticate_user!
 		file = params[:photo].delete(:file).read
 
 		@photo = Photo.new(params[:photo])
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 module ApplicationHelper
-  CAPTIONS = ["Future heart breaker.", "Do I look pleased?", "They see me rollin'.",
-               "I'd love me some cookies.'", "I just farted."]
+  CAPTIONS = ["This caption isn't funny.",
+              "I'm hungry for cookies.",
+              "Future heart breaker",
+              "I hope Morgan has better ideas than me.",
+              "Is this train ride as boring for you as it is for me?"]
 end
